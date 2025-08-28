7685291bc6e9b0d35dc3df69ebee05c41135559e
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-08-28 17:11:02 +0530

    Create ProjectInstaller.cs

---
+++ ProjectInstaller.cs
@@ -0,0 +1,111 @@
+//namespace MyWindowsService
+//{
+//    partial class ProjectInstaller
+//    {
+//        /// <summary>
+//        /// Required designer variable.
+//        /// </summary>
+//        private System.ComponentModel.IContainer components = null;
+
+//        /// <summary> 
+//        /// Clean up any resources being used.
+//        /// </summary>
+//        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing && (components != null))
+//            {
+//                components.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+
+//        #region Component Designer generated code
+
+//        /// <summary>
+//        /// Required method for Designer support - do not modify
+//        /// the contents of this method with the code editor.
+//        /// </summary>
+//        private void InitializeComponent()
+//        {
+//            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+//            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+//            this.serviceProcessInstaller1 = System.ServiceProcess.ServiceAccount.LocalSystem;
+//            // 
+//            // serviceProcessInstaller1
+//            // 
+//            this.serviceProcessInstaller1.Password = null;
+//            this.serviceProcessInstaller1.Username = null;
+//            // 
+//            // serviceInstaller1
+//            // 
+//            this.serviceInstaller1.Description = "My First Window Service";
+//            this.serviceInstaller1.DisplayName = "MyWindowService";
+//            this.serviceInstaller1.ServiceName = "Service1";
+//            // 
+//            // ProjectInstaller
+//            // 
+//            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+//            this.serviceProcessInstaller1,
+//            this.serviceInstaller1});
+
+//        }
+
+//        #endregion
+
+//        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+//        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+//    }
+//}
+
+namespace MyWindowsService
+{
+    partial class ProjectInstaller
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "My First Windows Service";
+            this.serviceInstaller1.DisplayName = "MyWindowsService";
+            this.serviceInstaller1.ServiceName = "Service1";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+                this.serviceProcessInstaller1,
+                this.serviceInstaller1});
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
